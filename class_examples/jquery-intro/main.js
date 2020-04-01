4229085efc7e2a02d0e340c0f52f4308fd7b0404
rlc132 <rlc132@miami.edu>
2020-04-01 13:34:07 -0400

    j
    jjj

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
 //all js goes in here
   console.log("we loaded");
 
-$("#title").css("color", "#bada55");
-$("#title").html("More information #1");
+$("#title").click(function() {
+  // code that runs when you clock on the part of the page that has id title.
+  // $("#title").css("color", "#f9423a");
+  // $("#title").css("font-size", "500px");
+  $("#title").addClass("big-text");
+});
+
+$("button2").click(function() {
+  // stuff we put here will happen when you click button2.
+$("#title").removeClass("big-text");
+});
+
+$("p").mouseover(function() {
+  $("p").css("font-size", "21px");
+});
+
+$("p").mouseleave(function() {
+  $("p").css("background-color", "yellow");
+  $("p").css("font-size", "16px");
+});
+// $("#title").css("font-size", "500px");
+// $("#title").html("More information #1");
+
 });
