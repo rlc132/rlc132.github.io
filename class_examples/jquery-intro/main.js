3bb21f599e7206f0336616f06275a1c89f3ee6c9
rlc132 <rlc132@miami.edu>
2020-04-15 01:56:44 -0400

    stuck on j
    jjjjjjjjjjjjjjjjjjj

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
   console.log("we loaded");
 
 $("#button1")
-.click(function() {
-$("#title").click(function() {
-    $("#title").html("How To Make A Medianoche Cuban Sandwich.");
-});
+
   // code that runs when you clock on the part of the page that has id title.
   // $("#title").css("color", "#f9423a");
   // $("#title").css("font-size", "500px");
@@ -14,28 +11,22 @@ $("#title").click(function() {
 });
 
 $("button2").click(function() {
+
   // stuff we put here will happen when you click button2.
 });
 
-$("#box").mousedown(function() {
-  $("#box").css("background-color", "green");
-});
 
-$("#finishedpic").mouseover(function(){
-  $("body").css("background-color", "#B0BF1A");
-});
-
-// $("p").mouseover(function() {
-//   $("p").css("font-size", "21px");
-// });
+ $("p").mouseover(function() {
+  $("p").css("font-size", "21px");
+ });
 
 
 
 
-//   $("p").css("background-color", "yellow");
-//   $("p").css("font-size", "16px");
-// });
-// $("#title").css("font-size", "500px");
-// $("#title").html("More information #1");
+ $("p").css("background-color", "yellow");
+   $("p").css("font-size", "16px");
+ });
+ $("#title").css("font-size", "500px");
+ $("#title").html("More information #1");
 
 });
--- hw/jquery/js/main.js
+++ hw/jquery/js/main.js
@@ -2,37 +2,42 @@ $(document).ready(function() {
   //all js goes in here
   console.log("we loaded");
 
-$("h1").doStuff();
+$("title").mouseover( function() {
+  // when your mouse is over a paragraph, the font size will become 21px
+  $("p")
+    .css("font-size", "21px");
 
-$( "#button1" )
+$("#button1")
 			.click( function() {
 				// Code that runs when you click on the part of the page that has id="button1".
-				$( "#title" )
-					.addClass( "big-text" );
-			} );
+        // $("#show").click(function(){
+        //  $("h2").show();
+        // });
+        $("p")
+          .css("font-size", "21px")
+			});
 
-		$( "#button2" )
+$("#button2")
 			.click( function() {
 				// stuff we put here will happen when you click button2.
-				// in this case we remove the class called big-text from
-				// the part of the page that has id=title
-				$( "#title" )
-					.removeClass( "big-text" );
-			} );
+        $("#hide").click(function(){
+  $("h2").hide();
+});
+});
 
-      $( "p" )
+      $("p")
 			.mouseover( function() {
 				// when your mouse is over a paragraph, the font size will become 21px
-				$( "p" )
-					.css( "font-size", "21px" );
-			} );
-      $( "p" )
+				$("p")
+					.css("font-size", "21px");
+			});
+      $("p")
       			.mouseleave( function() {
       				// when your mouse leaves a paragraph the font-size will get smaller
       				// and the background-color of the paragraph will become yellow
-      				$( "p" )
-      					.css( "background-color", "yellow" );
-      				$( "p" )
-      					.css( "font-size", "16px" );
-      			} );
-            	} );
+      				$("p")
+      					.css("background-color", "yellow");
+      				$("p")
+      					.css("font-size", "16px");
+      			});
+            	});
